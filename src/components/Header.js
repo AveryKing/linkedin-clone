c46c1f1ee8288edbac62ae93d5625882bdbbbabf
Avery King <avery.n.king@wmich.edu>
2022-03-25 04:31:59 -0400

    header structured

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
         <div>
             <div className="header">
                 <h1>header</h1>
+                <div className="header__left">
+                    
+                </div>
+                <div className="header__right">
+
+                </div>
             </div>
         </div>
     );
