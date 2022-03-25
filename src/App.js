dc5a4c6c1567d1524ee40b2a7ebb57ae8b9a68ae
Avery King <avery.n.king@wmich.edu>
2022-03-25 05:25:42 -0400

    style updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './styles/App.css';
 import { Counter } from './features/counter/Counter';
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
