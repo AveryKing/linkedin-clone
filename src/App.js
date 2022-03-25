6e41eabbd63ab6545a2976c9efbee07bcf12cabb
Avery King <avery.n.king@wmich.edu>
2022-03-25 05:16:21 -0400

    Sidebar component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Counter } from './features/counter/Counter';
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
