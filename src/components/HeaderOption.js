b04a28c83dd6fa17a5c2179d15f1bd82f7bd592b
Avery King <avery.n.king@wmich.edu>
2022-03-25 05:11:28 -0400

    Avatar added to HeaderOption

--- src/components/HeaderOption.js
+++ src/components/HeaderOption.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../styles/HeaderOption.css';
+import {Avatar} from "@mui/material";
 const HeaderOption = ({avatar, Icon,text}) => {
     return (
         <div>
             <div className="headerOption">
                 {Icon && <Icon className='headerOption__icon'/> }
+                {avatar && (
+                    <Avatar src={avatar} className='headerOption__icon' />
+                )}
                 <h3 className='headerOption__title'>{text}</h3>
             </div>
         </div>
