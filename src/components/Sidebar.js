30d9cdbef4426ff87d9ed849e34d58c7c9038407
Avery King <avery.n.king@wmich.edu>
2022-03-25 05:23:19 -0400

    sidebar__top structured

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import '../styles/Sidebar.css';
 import {Avatar} from "@mui/material";
+
 const Sidebar = () => {
     return (
         <div>
             <div className="sidebar__top">
-                <img src="" alt="" />
-                <Avatar className='sidebar__avatar' />
+                <img src="" alt=""/>
+                <Avatar className='sidebar__avatar'/>
                 <h2>Avery King</h2>
                 <h4>averyking.com</h4>
             </div>
             <div className="sidebar__stats">
                 <div className="sidebar__stat">
-                    
+                    <p>Who viewed you</p>
+                    <p className="sidebar__statNumber">1,337</p>
                 </div>
                 <div className="sidebar__stat">
-
+                    <p>Views on post</p>
+                    <p className="sidebar__statNumber">4,269</p>
                 </div>
-
             </div>
             <div className="sidebar__bottom">
-
+                <p>Recent</p>
             </div>
         </div>
     );
