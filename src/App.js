aebc5be983161d2e002c385b5808d6e5ebeca884
Avery King <avery.n.king@wmich.edu>
2022-03-25 04:31:28 -0400

    css reset

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import { Counter } from './features/counter/Counter';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import '../styles/Header.css';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header">
+                <h1>header</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
