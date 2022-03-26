801217456500e0473772ba1643e18d9da4428d37
Avery King <avery.n.king@wmich.edu>
2022-03-25 20:49:30 -0400

    input option icons added to feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import CreateIcon from '@mui/icons-material/Create'
+import ImageIcon from '@mui/icons-material/Image'
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions'
+import EventNoteIcon from '@mui/icons-material/EventNote'
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay'
 import '../styles/Feed.css';
 import InputOption from "./InputOption";
 const Feed = () => {
@@ -14,7 +18,10 @@ const Feed = () => {
                     </form>
                 </div>
                 <div className="feed__inputOptions">
-                    <InputOption />
+                    <InputOption Icon={ImageIcon} text='Photo' color='#70B5F9'/>
+                    <InputOption Icon={SubscriptionsIcon} text='Video' color='#E7A33E'/>
+                    <InputOption Icon={EventNoteIcon} text='Event' color='#C0CBCD'/>
+                    <InputOption Icon={CalendarViewDayIcon} text='Write Article' color='#7FC15E'/>
                 </div>
             </div>
         </div>
