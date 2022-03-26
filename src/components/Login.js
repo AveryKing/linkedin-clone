f10057c49f44945e03a660ba0d8249a0c7088c8e
Avery King <avery.n.king@wmich.edu>
2022-03-26 00:08:07 -0400

    register function complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/Login.css';
+import {auth} from "../lib/firebase";
+import {useDispatch} from "react-redux";
+import {login} from "../redux/userSlice";
+
 const Login = () => {
-    const register = () => {}
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('')
+    const [profilePicture, setProfilePicture] = useState('');
+    const dispatch = useDispatch();
+    const register = (e) => {
+        e.preventDefault();
+        if (!name) return alert('Please enter a name');
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((userAuth) => {
+                userAuth.user.updateProfile({
+                    photoURL: profilePicture,
+                    displayName: name
+                })
+                    .then(() => {
+                        dispatch(login({
+                            email: userAuth.user.email,
+                            uid: userAuth.user.uid,
+                            displayName:name,
+                            photoURL:profilePicture
+                        }))
+                    })
+            })
+            .catch((err) => {
+                alert(err.message)
+            })
+
+    }
+    const loginToApp = (e) => {
+
+    }
     return (
         <div className='login'>
-            <img src="https://www.paperlesslabacademy.com/wp-content/uploads/2017/02/linkedin-logo-transparent.png" alt=""/>
-            <form>
-                <input placeholder="Full name" type="text"/>
-                <input placeholder="Profile pic URL (optional)" type="text"/>
-                <input placeholder="Email" type="email"/>
-                <input placeholder="Password" type="password"/>
+            <img src="https://www.paperlesslabacademy.com/wp-content/uploads/2017/02/linkedin-logo-transparent.png"
+                 alt=""/>
+            <form onSubmit={register}>
+                <input value={name} onChange={e => setName(e.target.value)} placeholder="Full name" type="text"/>
+                <input value={profilePicture} onChange={e => setProfilePicture(e.target.value)}
+                       placeholder="Profile pic URL (optional)" type="text"/>
+                <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" type="email"/>
+                <input value={password} onChange={e => setPassword(e.target.value)} placeholder="Password"
+                       type="password"/>
                 <button type='submit'>Sign In</button>
             </form>
             <p>Not a member?{" "}
-            <span className='login__registerNow' onClick={register}>Register Now</span></p>
+                <span className='login__registerNow' onClick={register}>Register Now</span></p>
         </div>
     );
 };
