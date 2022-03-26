612d96f32eaf1ab5ad60d3cf303cf2b19df51eb3
Avery King <avery.n.king@wmich.edu>
2022-03-25 23:22:45 -0400

    Login component added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './styles/App.css';
-import { Counter } from './redux/Counter';
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
+import {useSelector} from "react-redux/lib/hooks/useSelector";
+import {selectUser} from "./redux/userSlice";
+import Login from "./components/Login";
 
-function App() {
+function App() {const user = useSelector(selectUser)
   return (
     <div className="app">
       <Header/>
-        <div className="app__body">
-            <Sidebar />
-            <Feed/>
-        </div>
+        {user ? (
+            <div className="app__body">
+                <Sidebar />
+                <Feed/>
+            </div>
+        ) : (
+            <Login />
+        )}
+
     </div>
   );
 }
