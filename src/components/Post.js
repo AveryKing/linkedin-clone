2c462c4dd6ee9dd1afabd50505aec4ea480cc7b8
Avery King <avery.n.king@wmich.edu>
2022-03-25 20:58:58 -0400

    post component added

---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Post.css';
+const Post = ({name,description,message,photoUrl}) => {
+    return (
+        <div className='post'>
+            
+        </div>
+    );
+};
+
+export default Post;
