692c3d79405b0a692e2de62857db156718173888
Avery King <avery.n.king@wmich.edu>
2022-03-26 03:09:05 -0400

    Login component redesigned to include Google and Github buttons

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,14 @@ import '../styles/Login.css';
 import {auth} from "../lib/firebase";
 import {useDispatch} from "react-redux";
 import {login} from "../redux/userSlice";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import {FcGoogle} from 'react-icons/fc';
+import {GrGithub} from 'react-icons/gr';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {Divider} from "@mui/material";
 
 const Login = () => {
+    const [mode, setMode] = useState(1);
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('')
@@ -51,17 +56,39 @@ const Login = () => {
         <div className='login'>
             <img src="https://www.paperlesslabacademy.com/wp-content/uploads/2017/02/linkedin-logo-transparent.png"
                  alt=""/>
+            <div className="login__text">
+                <p className='login__message'>In a hurry?<br/><span>Click here to use demo account</span></p>
+            </div>
+
             <form onSubmit={loginToApp}>
-                <input  autoComplete="none" value={name} onChange={e => setName(e.target.value)} placeholder="Full name" type="text"/>
-                <input autoComplete={"none" } value={profilePicture} onChange={e => setProfilePicture(e.target.value)}
-                       placeholder="Profile pic URL (optional)" type="text"/>
+                {mode === 2 && (
+                    <>
+                        <input  autoComplete="none" value={name} onChange={e => setName(e.target.value)} placeholder="Full name" type="text"/>
+                        <input autoComplete={"none" } value={profilePicture} onChange={e => setProfilePicture(e.target.value)}
+                               placeholder="Profile pic URL (optional)" type="text"/>
+                    </>
+                )}
                 <input autoComplete={"none" } value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" type="email"/>
                 <input autoComplete={"none" } value={password} onChange={e => setPassword(e.target.value)} placeholder="Password"
                        type="password"/>
                 <button type='submit'>Sign In</button>
+                <Divider className='login__divider'>OR</Divider>
+                <div onClick={() => null}
+                     className='login__google'>
+                    <FcGoogle size={25}/>
+                    <p>Sign In With Google</p>
+                </div>
+                <div onClick={() => null}
+                     className='login__google'>
+                    <GrGithub size={25}/>
+                    <p>Sign In With GitHub</p>
+                </div>
             </form>
-            <p>Not a member?{" "}
+            <p className='login__notMember'>Not a member?{" "}
                 <span className='login__registerNow' onClick={register}>Register Now</span></p>
+            <footer>
+                Created with <FavoriteIcon sx={{position:'relative',fontSize:'20px',top:'5px',color:'#ff004d'}} /> by Avery King
+            </footer>
         </div>
     );
 };
