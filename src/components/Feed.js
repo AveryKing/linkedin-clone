be6996609af7ab1c3c9cfcdb9a81a36bfcbafc98
Avery King <avery.n.king@wmich.edu>
2022-03-25 22:09:53 -0400

    firestore querySnapshot for posts added to useEffect

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import CreateIcon from '@mui/icons-material/Create'
 import ImageIcon from '@mui/icons-material/Image'
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions'
@@ -7,8 +7,24 @@ import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay'
 import '../styles/Feed.css';
 import InputOption from "./InputOption";
 import Post from "./Post";
+import {db} from '../lib/firebase';
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+
 const Feed = () => {
     const [posts, setPosts] = useState([1,2,2,2,2,2,2]);
+
+    useEffect(() => {
+        onSnapshot(collection(db, 'posts'), (snapshot) => {
+            const posts = [];
+            snapshot.forEach((doc) => (
+                posts.push({
+                    id:doc.id,
+                    data:doc.data()
+                })
+            ))
+        })
+    },[])
+
     const sendPost = (evt) => {
         evt.preventDefault();
     }
@@ -29,8 +45,9 @@ const Feed = () => {
                     <InputOption Icon={CalendarViewDayIcon} text='Write Article' color='#7FC15E'/>
                 </div>
             </div>
-            {posts.map((post) => (
-                <Post name='Avery King'
+            {posts.map((post,index) => (
+                <Post key={index}
+                      name='Avery King'
                       description='this is a test'
                       message='it works!'
                       />
