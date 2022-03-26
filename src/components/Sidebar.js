17428f1729b4fa51b3f4373f0ad638eb82232285
Avery King <avery.n.king@wmich.edu>
2022-03-25 20:07:40 -0400

    profile banner added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,7 +6,7 @@ const Sidebar = () => {
     return (
         <div className='sidebar'>
             <div className="sidebar__top">
-                <img src="" alt=""/>
+                <img src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2VhJTIwYmVhY2h8ZW58MHx8MHx8&w=1000&q=80" alt=""/>
                 <Avatar className='sidebar__avatar'/>
                 <h2>Avery King</h2>
                 <h4>avery@king.com</h4>
